efd70f8130c234df74f9dd1dfc54d9775a1d2641
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-03-19 17:09:18 -0300

    Feat: Conexão com o DB.

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectToDatabase = require('./src/database/database');
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+
+connectToDatabase();
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'Bem-vindo ao APP da Duarte Pizzaria'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em: http://localhost:${port}`);
+});
