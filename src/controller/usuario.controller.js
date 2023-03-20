8647207b6c38cc9be538911b439e2757faed8e2c
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-03-20 07:47:02 -0300

    Feat: User controller add e rotas atualizadas.

---
+++ src/controller/usuario.controller.js
@@ -0,0 +1,93 @@
+
+const findUserByIdController = async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+const findAllUsersController = async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+const createUserController = async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+const updateUserController = async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+const removeUserController = async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+const addUserAddressController = async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+const removeUserAddressController = async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+const addUserFavPizza = async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+const removeUserFavPizza = async (req, res) => {
+    try {
+
+    } catch (err) {
+        console.log(`erro: ${err.message}`);
+        return res.status(500).send({ message: `Erro inesperado, tente novamente.` });
+    }
+}
+
+module.exports = {
+    findUserByIdController,
+    findAllUsersController,
+    createUserController,
+    updateUserController,
+    removeUserController,
+    addUserAddressController,
+    removeUserAddressController,
+    addUserFavPizza,
+    removeUserFavPizza
+}
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controller/usuario.controller');
 
-router.get('/findById/:id');
-router.get('/findAll');
+router.get('/findById/:id', usuarioController.findUserByIdController);
+router.get('/findAll', usuarioController.findAllUsersController);
 
-router.post('/create');
-router.post('/addAddress/:id');
-router.post('/addFavPizza/:id');
+router.post('/create', usuarioController.createUserController);
+router.post('/addAddress/:id', usuarioController.addUserAddressController);
+router.post('/addFavPizza/:id', usuarioController.addUserFavPizza);
 
-router.put('/update/:id');
+router.put('/update/:id', usuarioController.updateUserController);
 
-router.delete('/remove/:id');
-router.delete('/removeAddress');
-router.delete('/removeFavPizza');
+router.delete('/remove/:id', usuarioController.removeUserController);
+router.delete('/removeAddress', usuarioController.removeUserAddressController);
+router.delete('/removeFavPizza', usuarioController.removeUserFavPizza);
 
 module.exports = router;
