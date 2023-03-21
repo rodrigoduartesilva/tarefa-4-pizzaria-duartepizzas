31a8c4bca07f8058115397afd920dea60b789155
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-03-21 19:30:07 -0300

    Fix: Carrinho, corpo da requisição.

--- src/model/Carrinho.js
+++ src/model/Carrinho.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const CarrinhoSchema = new mongoose.Schema({
     produtos: [
         {
-            _id: { type: mongoose.Schema.Types.ObjectId, ref: 'produtos' },
+            _id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'produtos' },
             quantidade: { type: Number, required: true }
         },
     ],
     createdAt: { type: Date, required: true, default: Date.now() },
     precoTotal: { type: Number, required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'produtos' }
+    userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'usuarios' }
 });
 
 const Carrinho = mongoose.model('carrinhos', CarrinhoSchema);
