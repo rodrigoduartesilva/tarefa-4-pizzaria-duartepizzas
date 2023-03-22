e98fea66b757acbe1fe0da9fed1a40d7eb902038
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-03-22 10:55:47 -0300

    Feat: Validação carrinho e pedido.

--- src/middleware/validacao.middleware.js
+++ src/middleware/validacao.middleware.js
@@ -66,9 +66,55 @@ const validaCategoria = (req, res, next) => {
     return next();
 }
 
+const validaPedido = (req, res, next) => {
+    let erros = [];//array para acumular os erros
+
+    if (!req.body.precoTotal) {
+        erros.push('precoTotal');
+    }
+
+    if (!req.body.concluido) {
+        erros.push('concluido');
+    }
+
+    //Teste de quantos erros ocorreram
+    if (erros.length == 0) {
+        return next();
+    } else {
+        if (erros.length > 1) {
+            return res.status(400).send({ message: `Os campos ${erros} precisam ser preenchidos.` });
+        } else {
+            return res.status(400).send({ message: `O campo ${erros} precisa ser preenchido.` });
+        }
+    }
+}
+
+
+const validaCarrinho = (req, res, next) => {
+    let erros = [];//array para acumular os erros
+
+    if (!req.body.precoTotal) {
+        erros.push('precoTotal');
+    }
+
+    //Teste de quantos erros ocorreram
+    if (erros.length == 0) {
+        return next();
+    } else {
+        if (erros.length > 1) {
+            return res.status(400).send({ message: `Os campos ${erros} precisam ser preenchidos.` });
+        } else {
+            return res.status(400).send({ message: `O campo ${erros} precisa ser preenchido.` });
+        }
+    }
+}
+
+
 
 module.exports = {
     validaUsuario,
     validaPizza,
-    validaCategoria
+    validaCategoria,
+    validaPedido,
+    validaCarrinho
 }
--- src/router/carrinho.router.js
+++ src/router/carrinho.router.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const authMiddleware = require('../middleware/auth.middleware');
+const { validaCarrinho } = require('../middleware/validacao.middleware');
 
 const carrinhoController = require('../controller/carrinho.controller');
 
 router.get('/find/:id', authMiddleware, carrinhoController.findCarrinhoByIdController);
 router.get('/findAll', authMiddleware, carrinhoController.findAllCarrinhosController);
 
-router.post('/create', authMiddleware, carrinhoController.createCarrinhoController);
+router.post('/create', authMiddleware, validaCarrinho, carrinhoController.createCarrinhoController);
 
-router.put('/update/:id', authMiddleware, carrinhoController.updateCarrinhoController);
+router.put('/update/:id', authMiddleware, validaCarrinho, carrinhoController.updateCarrinhoController);
 
 router.delete('/delete/:id', authMiddleware, carrinhoController.deleteCarrinhoController);
 
