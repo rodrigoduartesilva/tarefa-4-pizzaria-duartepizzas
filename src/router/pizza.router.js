e6eebe0df0a4591f775ceea3f5de9825cf449611
Rodrigo Duarte <rodrigoduartesilva@yahoo.com.br>
2023-03-26 12:55:04 -0300

    Fix: Modificação dos nomes das rotas em pizza.

--- src/router/pizza.router.js
+++ src/router/pizza.router.js
@@ -4,7 +4,7 @@ const pizzaController = require('../controller/pizza.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 const { validaPizza, validaId } = require('../middleware/validacao.middleware');
 
-router.get('/find/:id', authMiddleware, validaId, pizzaController.findPizzaByIdController);
+router.get('/findById/:id', authMiddleware, validaId, pizzaController.findPizzaByIdController);
 router.get('/findAll', authMiddleware, pizzaController.findAllPizzasController);
 
 router.post('/create', authMiddleware, validaPizza, pizzaController.createPizzaController);
@@ -12,7 +12,7 @@ router.post('/addCategoria/:id', authMiddleware, validaId, pizzaController.addCa
 
 router.put('/update/:id', authMiddleware, validaId, validaPizza, pizzaController.updatePizzaController);
 
-router.delete('/delete/:id', authMiddleware, validaId, pizzaController.deletePizzaController);
+router.delete('/remove/:id', authMiddleware, validaId, pizzaController.deletePizzaController);
 router.delete('/removeCategoria/:id', authMiddleware, pizzaController.removeCategoriaPizzaController);
 
 module.exports = router;
